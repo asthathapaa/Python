833e857825c1c878b4a226347a4a31146d506b52
Astha Thapa <119739490+asthathapaa@users.noreply.github.com>
2024-05-01 09:08:38 +0545

    Add files via upload

---
+++ 1_intro.py
@@ -0,0 +1,155 @@
+Python 3.12.3 (tags/v3.12.3:f6650f9, Apr  9 2024, 14:05:25) [MSC v.1938 64 bit (AMD64)] on win32
+Type "help", "copyright", "credits" or "license()" for more information.
+print("Astha")
+Astha
+if 10>2:
+    print("10 is greater")
+
+    
+10 is greater
+if(12>6):
+    print("12 is greater");
+    else
+    
+SyntaxError: invalid syntax
+
+else:
+    
+SyntaxError: invalid syntax
+if(%>@):
+    
+SyntaxError: invalid syntax
+if(*>+)
+SyntaxError: Invalid star expression
+if(*>+):
+    
+SyntaxError: Invalid star expression
+if(a>b):
+    print("a is greater");
+    if(c>d):
+        printf("b is greater");
+
+        
+Traceback (most recent call last):
+  File "<pyshell#16>", line 1, in <module>
+    if(a>b):
+NameError: name 'a' is not defined
+int a=10,b=3,c=12,d=6;
+SyntaxError: invalid syntax
+if(a>b):
+    print("a is greater");
+    if(c>d):
+        printf("b is greater");
+
+Traceback (most recent call last):
+  File "<pyshell#18>", line 1, in <module>
+    if(a>b):
+NameError: name 'a' is not defined
+if(a=10>b=2):
+    print("a is greater");
+    if(c=5>d=2):
+        printf("b is greater");
+        
+SyntaxError: invalid syntax. Maybe you meant '==' or ':=' instead of '='?
+
+exit()
+if 5>2:
+    print("5 largest")
+    if 5>2:
+        print("5 is largest")
+
+        
+5 largest
+5 is largest
+//variable
+SyntaxError: invalid syntax
+#variable
+x=5
+y="Hello World"
+
+print(x)
+5
+""" This is a comment """
+' This is a comment '
+print(type(x))
+<class 'int'>
+print(type(y))
+<class 'str'>
+a=4
+A="USA"
+print(a,A)
+4 USA
+myvar= "John Doe"
+my_var="John Doe"
+_my_var="JOhn Doe"
+myVariableName="Joh"
+x,y,x="Orange","aPPLE"
+Traceback (most recent call last):
+  File "<pyshell#43>", line 1, in <module>
+    x,y,x="Orange","aPPLE"
+ValueError: not enough values to unpack (expected 3, got 2)
+X,Y,Z="USA","UK","AUS"
+print(X,Y,Z)
+USA UK AUS
+x=y=z="AT"
+print(x)
+AT
+print(x,y,x)
+AT AT AT
+fruits=["apple","bannana","guavae"]
+x,y,z= fruits
+print(x,y,z)
+apple bannana guavae
+x="Python is love"
+print(x)
+Python is love
+print(y+z)
+bannanaguavae
+x=110
+y=6
+print(x+y)
+116
+x ="astha"
+def asthaf():
+    print("Python by"+x)
+...     asthaf()
+... 
+...     
+>>> 
+>>> def myfun():
+...     print(x)
+...     myfun()
+... 
+...     
+>>> a="rule"
+>>> def xyz():
+...     print("I used to"+a)
+... 
+...     
+>>> xyz()
+I used torule
+>>> x="viva"
+>>> 
+>>> def fun():
+...     x="de la vida"
+...     print("Music="+ x)
+... 
+...     
+>>> fun()
+Music=de la vida
+>>> print("Music="+x)
+Music=viva
+>>> 
+>>> def funcn():
+...     global a
+...     a="winner takes it all"
+... 
+...     
+>>> funn()
+Traceback (most recent call last):
+  File "<pyshell#86>", line 1, in <module>
+    funn()
+NameError: name 'funn' is not defined. Did you mean: 'fun'?
+>>> funcn()
+>>> print(a)
+winner takes it all
